0c15b54423739fef132ab99342a1417f1ab7e12c
Thomas Chamberlin <tchamber@nrao.edu>
2015-04-20 15:31:30 -0400

    Added licensing information

--- src/fake_gpu_thread.c
+++ src/fake_gpu_thread.c
@@ -1,3 +1,25 @@
+//# Copyright (C) 2015 Associated Universities, Inc. Washington DC, USA.
+//#
+//# This program is free software; you can redistribute it and/or modify
+//# it under the terms of the GNU General Public License as published by
+//# the Free Software Foundation; either version 2 of the License, or
+//# (at your option) any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but
+//# WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//# General Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License
+//# along with this program; if not, write to the Free Software
+//# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning GBT software should be addressed as follows:
+//# GBT Operations
+//# National Radio Astronomy Observatory
+//# P. O. Box 2
+//# Green Bank, WV 24944-0002 USA
+
 // run with:
 // $ clean_ipc; taskset 0x0606 hashpipe -p fake_gpu -I 0 -o BINDHOST=px1-2.gb.nrao.edu -o GPUDEV=0 -o XID=0 -c 3 fake_gpu_thread -c 2 fake_gpu_test_thread
 
